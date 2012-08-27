176552665b235a323f193ae572749395041f2c7c
alejandroperez <alejandrojorgeperez@gmail.com>
2012-08-27 16:46:06 -0700

    Changed license for FacetNameTest after checking with the author (Hao Yan).

--- bobo-browse/src/test/java/com/browseengine/bobo/test/FacetNameTest.java
+++ bobo-browse/src/test/java/com/browseengine/bobo/test/FacetNameTest.java
@@ -1,26 +1,20 @@
 /**
- * Bobo Browse Engine - High performance faceted/parametric search implementation 
- * that handles various types of semi-structured data.  Written in Java.
- * 
- * Copyright (C) 2005-2006  John Wang
+ * This software is licensed to you under the Apache License, Version 2.0 (the
+ * "Apache License").
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * LinkedIn's contributions are made under the Apache License. If you contribute
+ * to the Software, the contributions will be deemed to have been made under the
+ * Apache License, unless you expressly indicate otherwise. Please do not make any
+ * contributions that would be inconsistent with the Apache License.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * You may obtain a copy of the Apache License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, this software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Apache
+ * License for the specific language governing permissions and limitations for the
+ * software governed under the Apache License.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * To contact the project administrators for the bobo-browse project, 
- * please go to https://sourceforge.net/projects/bobo-browse/, or 
- * send mail to owner@browseengine.com.
+ * © 2012 LinkedIn Corp. All Rights Reserved.  
  */
 
 package com.browseengine.bobo.test;
